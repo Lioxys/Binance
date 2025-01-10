e9beeae54fbc65ff4134d1814ae7ca13a6ac564e
Lioxys <ludkas.pilorge@gmail.com>
2025-01-10 10:57:27 +0100

    ajout page cryptos

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,10 +1,22 @@
-const BASE_URL = 'https://api.coinlore.net/api';
+const COINGECKO_BASE_URL = 'https://api.coingecko.com/api/v3';
 
 export async function fetchCryptoById(coinId) {
-  const response = await fetch(`${BASE_URL}/ticker/?id=${coinId}`);
-  if (!response.ok) {
-    throw new Error(`Erreur HTTP ! status: ${response.status}`);
+    const response = await fetch(`${COINGECKO_BASE_URL}/coins/${coinId}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`);
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
   }
-  const data = await response.json();
-  return data[0];
-}
+
+export async function fetchCryptoHistory(coinId, days = 30) {
+    const response = await fetch(`${COINGECKO_BASE_URL}/coins/${coinId}/market_chart?vs_currency=usd&days=${days}`);
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.prices.map(item => ({
+      date: new Date(item[0]).toLocaleDateString(),
+      price: item[1],
+    }));
+  }
